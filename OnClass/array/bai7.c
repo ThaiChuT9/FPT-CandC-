836d67c9b53ab0e60cddba59e92b3cda3b1bbb59
ThaiChuT9 <iamtk4i@gmail.com>
2024-04-24 08:51:34 +0700

    array2

---
+++ OnClass/array/bai7.c
@@ -0,0 +1,22 @@
+﻿//
+// Created by THAICHU on 4/24/2024.
+//
+#include <stdio.h>
+int babaii7(){
+//int main(){
+    char c[10];
+    int i;
+    printf("Nhap ky toi da 10 ky tu\n");
+    for (i = 0; i < 10; ++i) {
+        printf("Nhap ky tu thu %d:",i+1);
+        scanf("%c", &c[i]);
+        fflush(stdin);
+        if (c[i] == '\n'){
+            break;
+        }
+    }
+    printf("Ky tu vua nhap la: \n");
+    for (int j = 0; j < i; ++j) {
+        printf("%c %d\n", c[j],c[j]);
+    }
+}
---
+++ OnClass/array/bai8.c
@@ -0,0 +1,35 @@
+﻿//
+// Created by THAICHU on 4/24/2024.
+//
+#include <stdio.h>
+int main(){
+    float diem[5][6];
+    for (int i = 0; i < 5; ++i) {
+        printf("Nhap diem cua hoc sinh %d:\n ",i+1);
+        for (int j = 0; j < 6; ++j) {
+            printf("nhap diem mon %d: ", j+1);
+            scanf("%f",&diem[i][j]);
+            while (diem[i][j] < 0 || diem[i][j] > 10){
+                printf("Diem khong hop le, vui long nhap lai!\n");
+                printf("nhap lai diem mon %d: ", j+1);
+                scanf("%f", &diem[i][j]);
+            }
+        }
+    }
+    printf("Danh sach diem va diem trung binh cua cac hoc sinh:\n");
+    printf("+----------+--------+--------+--------+--------+--------+---------+\n");
+    printf("| Hoc sinh | Mon 1  | Mon 2  | Mon 3  | Mon 4  | Mon 5  | Diem TB |\n");
+    printf("+----------+--------+--------+--------+--------+--------+---------+\n");
+    for (int i = 0; i < 5; ++i) {
+        float diemTB = 0;
+        printf("|    %d     | ", i+1);
+        for (int j = 0; j < 6; ++j) {
+            printf("%.2f  | ", diem[i][j]);
+            diemTB += diem[i][j];
+            if (j == 5){
+                printf("%.2f  |\n", diemTB/6);
+            }
+        }
+    }
+    printf("+----------+--------+--------+--------+--------+--------+---------+\n");
+}
